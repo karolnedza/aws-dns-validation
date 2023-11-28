5d98374215da9f9a9a1948dbb63802d0e85f312e
Karol Nedza <karol@Karols-MacBook-Pro.local>
2023-11-28 16:36:28 +0100

    corrected variable

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 resource "aws_route53_record" "example" {
   for_each = {
     for dvo in var.aws_cert : dvo.domain_name => {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -14,9 +13,9 @@ resource "aws_route53_record" "example" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = aws_route53_zone.example.zone_id
- }
+  zone_id         = data.aws_route53_zone.example.zone_id
 }
+
 
 data "aws_route53_zone" "example" {
   name         = "avxlab.cc"
